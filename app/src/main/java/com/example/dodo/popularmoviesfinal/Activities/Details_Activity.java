6ce02d9568e60609f2756d7ad13dd83f25f84180
dina789 <120150340@sha.edu.eg>
2018-04-07 20:39:19 +0200

    Started the detail view (I already made this screen in another file)

--- app/src/main/java/com/example/dodo/popularmoviesfinal/Activities/Details_Activity.java
+++ app/src/main/java/com/example/dodo/popularmoviesfinal/Activities/Details_Activity.java
@@ -1,15 +1,66 @@
 package com.example.dodo.popularmoviesfinal.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
+import com.example.dodo.popularmoviesfinal.Models.MoviesData;
 import com.example.dodo.popularmoviesfinal.R;
+import com.squareup.picasso.Picasso;
 
 public class Details_Activity extends AppCompatActivity {
+    ImageView image_poster;
+    TextView text_release_date, text_vote_average, text_overview, text_original_title;
+    double rate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details_);
+
+        Intent intentGetMovieDetails  = getIntent();
+
+        MoviesData movieModel = (MoviesData) intentGetMovieDetails .getSerializableExtra("movieModel");
+
+
+
+        text_release_date = (TextView) findViewById(R.id.text_release_date);
+        //still setting the release date.
+
+        text_vote_average = (TextView) findViewById(R.id.text_vote_average);
+        text_overview = (TextView) findViewById(R.id.text_overview);
+        text_original_title = (TextView) findViewById(R.id.text_original_title);
+
+        RatingBar rating_bar=(RatingBar) findViewById(R.id.rating_bar);
+
+        text_original_title.setText(movieModel.getTitle()); //original title
+        text_overview .setText(movieModel.getOverview());  //overview
+        if (movieModel.getOverview() == "") {
+            text_overview.setText("Overview:\n Overview not available !!");
+        }
+
+
+      rate= movieModel.getVoteAverage(); //user rating
+        rate=rate/2;
+        rating_bar.setRating((float) rate);
+
+        String originDate = movieModel.getReleaseDate();
+
+
+        Picasso.with(getBaseContext()).load("http://image.tmdb.org/t/p/w500/" + movieModel.getPosterPath()).into(image_poster);
+
     }
-}
+    public void onBackButtonClick (View view)
+    {
+        //Back Code
+        Intent intent = new Intent(Details_Activity.this , MainActivity.class);
+        startActivity(intent);
+    }
+
+    }
+
