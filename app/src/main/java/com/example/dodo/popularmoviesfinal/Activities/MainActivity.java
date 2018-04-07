6fbac069ceaec5d15fc2ae6573294cd43b4806de
dina789 <120150340@sha.edu.eg>
2018-04-07 20:01:08 +0200

    Added setting and menu

--- app/src/main/java/com/example/dodo/popularmoviesfinal/Activities/MainActivity.java
+++ app/src/main/java/com/example/dodo/popularmoviesfinal/Activities/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.dodo.popularmoviesfinal.Adapters.MoviesAdapter;
@@ -121,6 +122,18 @@ recyclerView.setAdapter(moviesAdapter);
         return super.onCreateOptionsMenu(menu);
 
 
+    } @Override public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
